ffc1b468c7e448e5e25c494d7a7c92db9aa7cee5
peter <believeyourself@outlook.com>
2019-12-03 22:27:38 +0800

    提交IdGenerator

---
+++ src/main/java/com/weiquding/id/segment/IdGenerator.java
@@ -0,0 +1,29 @@
+package com.weiquding.id.segment;
+
+import java.util.List;
+
+/**
+ * 数据库批量段方案接口
+ *
+ * @author wuby
+ * @version V1.0
+ * @date 2019/12/3
+ */
+public interface IdGenerator<T> {
+
+    /**
+     * 生成单个ID
+     *
+     * @return ID
+     */
+    T generate();
+
+    /**
+     * 生成指定size个ID
+     *
+     * @param size 数量
+     * @return size个ID
+     */
+    List<T> generate(int size);
+
+}
